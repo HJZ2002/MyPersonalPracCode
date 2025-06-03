6ceb5e3a379ed43ac596a12253d66097db085fb1
HJZ2002 <97101416+HJZ2002@users.noreply.github.com>
2025-06-03 11:48:01 +0800

    Add files via upload

---
+++ Data_Structures_Algo/src/SavedAlgo/Bubble_Sort.java
@@ -0,0 +1,33 @@
+package SavedAlgo;
+
+public class Bubble_Sort {
+	public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 1, 5, 6};
+        System.out.println("Original array:");
+        printArray(arr);
+        bubbleSort(arr);
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+            System.out.print("After pass " + (i + 1) + ": ");
+            printArray(arr);
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
+
