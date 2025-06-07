4d76cd5851c6ff2f7b19bfaa930800838ef3c3e5
HJZ2002 <97101416+HJZ2002@users.noreply.github.com>
2025-06-08 02:35:24 +0800

    Add files via upload
    Trying to understand for every count and time of life todays time

---
+++ Codinglessons/src/lessons/CalendarV2.java
@@ -0,0 +1,20 @@
+package lessons;
+import java.util.Calendar;
+public class CalendarV2 {
+	public static void main (String[]args) {
+		Calendar calendar = Calendar.getInstance();
+		long timeInMillis = calendar.getTimeInMillis();
+		
+		Object obj = Calendar.getInstance();
+		long time = calendar.getTimeInMillis();
+		
+		if (obj instanceof Calendar) {
+			Calendar cal = (Calendar) obj;
+			System.out.println("Current time in milliseconds: " + cal.getTimeInMillis());
+		} else {
+			System.out.println("Object is not an instance of Calendar");
+		}
+		System.out.println("Current time in milliseconds: " + timeInMillis);
+		System.out.println("Current time in milliseconds: " + time);
+	}
+}
