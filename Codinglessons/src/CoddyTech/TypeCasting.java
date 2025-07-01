8061ca519eaf8447f88d726f1809def7ea5fa77d
HJZ2002 <97101416+HJZ2002@users.noreply.github.com>
2025-07-01 15:52:26 +0800

    Add files via upload

---
+++ Codinglessons/src/CoddyTech/TypeCasting.java
@@ -0,0 +1,17 @@
+package CoddyTech;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+	    // Declare and initialize variables
+        double price = 99.99;
+        int intPrice = 99;
+        
+        
+        // Output the values
+        System.out.println("Price: " + price);
+        System.out.println("Int Price: " + intPrice);
+        
+    }
+}
+
