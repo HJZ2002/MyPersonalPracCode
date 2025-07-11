a2386f4ee8ffa4d5eab3a5ab090e54432248072b
HJZ2002 <97101416+HJZ2002@users.noreply.github.com>
2025-07-11 23:59:18 +0800

    Add files via upload

---
+++ Codinglessons/src/CoddyTech/Scan_class.java
@@ -0,0 +1,19 @@
+package CoddyTech;
+import java.util.Scanner;
+public class Scan_class {
+	
+	public static void main(String[] args) {
+        // Create a Scanner object
+        Scanner scanner = new Scanner(System.in);
+        
+        // Prompt the user to enter their name
+         System.out.print("Enter your name: ");
+        
+        // Read the user's name
+        String name = scanner.nextLine();
+        
+        // Print the greeting message
+           System.out.println("Hello, " + name);
+    }
+}
+
