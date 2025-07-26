e38dcc96de25186af45bb8d33f3978effd464b54
HJZ2002 <97101416+HJZ2002@users.noreply.github.com>
2025-07-26 23:59:11 +0800

    Add files via upload

---
+++ Codinglessons/src/CoddyTech/Declare_Methods.java
@@ -0,0 +1,23 @@
+package CoddyTech;
+import java.util.Scanner;
+public class Declare_Methods {
+
+	public static void sumNumbers() {
+		int sum = 0;
+		for(int i = 1; i<= 1000; i++) {
+			sum += i;
+		}
+		System.out.println("" + sum);
+		
+	}
+	public static void main(String[]args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		
+		for(int i = 0; i < n; i++) {
+			sumNumbers();
+		}
+		scan.close();
+	}
+
+}
